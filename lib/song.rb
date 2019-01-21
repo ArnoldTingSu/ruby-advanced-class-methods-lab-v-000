81115c006ad00076d763869427cfcb39287db35e
Arnold Su <arnoldtingsu@gmail.com>
2019-01-21 07:59:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,28 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create ##constructor
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.find{|track| track.name == song_name}
+  end
+
   def self.all
     @@all
   end
@@ -9,5 +31,16 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+  
+  def self.alphabetical
+    self.all.sort_by{|a| a.name}
+  end
+  
 
 end
+
+##
